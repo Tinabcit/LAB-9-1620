4a4d9265956d7e274f63e480fff45ad036dbba48
Tina <tshabestari@my.bcit>
2021-03-12 22:22:13 -0800

    Add a json and changed to make a correct request

---
+++ json/apps.js
@@ -0,0 +1,21 @@
+num1 = 1
+num2 = 2
+const animal ='dog'
+const num4 = 2.24
+
+// Making constraint file to the tag and come with 2 string part
+const str1 = '3';
+const str2 = '2';
+
+// Make a if statement with a string attached to indicate the correct alert
+const str3 = '2';
+if(str3 === str2) alert('they match 2 equal 2')
+
+//Make a if statement with a string attached to indicate the not correct alert
+if(str1 === str2) alert('they match');
+else alert ('no luck 2 do not equal 3');
+
+
+for(let i=0; i <str1.length; i++){
+    console.log();
+}
